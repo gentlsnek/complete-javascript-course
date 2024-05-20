fc521c3a0bc26e7ef75eba499444d60004494fb9
erwinp <112609680+gentlsnek@users.noreply.github.com>
2024-05-20 17:38:36 +0530

    a

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,3 +1,29 @@
-'use strict';
+'use strict'
 
- 
+/*
+let age = 30;
+let oldAge = age;
+
+
+age = 31;
+
+
+//console.log(age);
+//console.log(oldAge);
+
+
+
+const me = {
+    name: 'erwin',
+    age: 30,
+};
+
+
+const friend = me;
+friend.age = 27;
+
+console.log('friend', friend);
+console.log('me', me);
+
+*/
+
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex){
+        return  [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,43 @@ const restaurant = {
     },
   },
 };
+
+
+/*
+const arr = [2,3,4];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+
+const [x,y,z] = arr;
+
+
+console.log(x , y, z);
+console.log(arr);
+
+
+
+let [main, ,secondary] = restaurant.categories;
+
+console.log(main, secondary);
+
+
+//to switch variables
+
+[secondary, main] = [main, secondary];
+
+console.log(main,  secondary);/.
+
+
+*/
+
+
+console.log(restaurant.order(2, 0));
+
+
+const[startercourse, maincourse] = restaurant.order(2,1);
+
+
+console.log(startercourse, maincourse);cxz
